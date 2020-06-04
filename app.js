9cac339abb09d7810705fcc0e758cb0904ac91d4
eylemyildiz <eylemyldz89@gmail.com>
2020-06-04 13:36:12 +0300

    isAuthenticated middleware

--- app.js
+++ app.js
@@ -18,7 +18,11 @@ const app = express();
 
 //console.log(process.env.NAME);
 
+//helpers
 const db = require('./helpers/db')();
+
+//middlewares
+const isAuthenticated = require('./middleware/isAuthenticated');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -47,7 +51,7 @@ app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/auth', authRouter);
-app.use('/chat', chatRouter);
+app.use('/chat',isAuthenticated, chatRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next)=> {
