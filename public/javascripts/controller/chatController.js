8d43c1f2cf6cafe8f8928c4a29473ddebe1ad7b2
eylemyildiz <eylemyldz89@gmail.com>
2020-06-04 15:58:07 +0300

    socket.io redis adapter

--- public/javascripts/controller/chatController.js
+++ public/javascripts/controller/chatController.js
@@ -1,4 +1,9 @@
 app.controller('chatController',['$scope',($scope) =>{
     const socket = io.connect("http://localhost:3000");
     console.log('aaaaaaaaaaaaaaaaaaaaaaaa hellooooo');
+
+    socket.on('hello', () =>{
+        console.log('hello from server1');
+    });
+
 }]);
