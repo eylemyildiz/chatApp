f0171c9b3ba18eeef1950a184bc7bae6e200086b
eylemyildiz <eylemyldz89@gmail.com>
2020-06-04 16:30:55 +0300

    chat tab yapisinin ayarlanmasi

--- public/javascripts/controller/chatController.js
+++ public/javascripts/controller/chatController.js
@@ -1,9 +1,15 @@
 app.controller('chatController',['$scope',($scope) =>{
+    $scope.activeTab = 2;
+
+    $scope.changeTab = tab =>{
+        $scope.activeTab = tab;
+    };
+
     const socket = io.connect("http://localhost:3000");
     console.log('aaaaaaaaaaaaaaaaaaaaaaaa hellooooo');
 
-    socket.on('hello', () =>{
+    /*socket.on('hello', () =>{
         console.log('hello from server1');
-    });
+    });*/
 
 }]);
